b9476d2efeea97489566fb5c62bbe02d6211523f
MOON <nobles92ts@gmail.com>
2026-01-28 20:16:39 +0900

    Tower, Bullet(임시) 관련 작업 내역 입니다. 이후에 TowerManager에서 생성, 삭제, 관리 등 작업 예정입니다.

---
+++ 9TeamTd/Assets/Data/Bullet.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // 테스트용 총알 함수 
+    // 발사 된 것을 구현하기 위해서 
+    private Transform _target; // 타겟
+    private float _speed = 20f; // 총알 속도
+    private float _damage; // 데미지
+    private Vector3 _direction; // 총알 방향 저장
+
+    void Start()
+    {
+        // 살아 남은 총알은 3초 후 자동 삭제 (이정도면 맵 밖에 나갈 거 같아서)
+        Destroy(gameObject,3f);
+    }
+
+    
+    void Update()
+    {
+        // 저장된 방향으로 이동
+        transform.position += _direction * _speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //타겟이 몬스터여야만함
+        if (other.CompareTag("Monster"))
+        {
+            //몬스터에게 데미지 주기 (몬스터 스크립트생기면 해주면됨)
+            // other.GetComponent<Monster>().TakeDamage(_damage);
+            
+            //총알 삭제
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetTarget(Transform target, float damage)
+    {
+        _target = target;
+        _damage = damage;
+
+        // 발사 순간 방향 저장
+        _direction = (target.position - transform.position).normalized;
+    }
+}
--- 9TeamTd/Assets/Tower/Tower.cs
+++ 9TeamTd/Assets/Tower/Tower.cs
@@ -2,17 +2,113 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// 자 이제 무엇을 할까요 
+
+// 타워의 기본 속성 및 기능 관리
+// 타워 사거리, 공격력, 공격속도, 타겟팅 방식 등 관리
+// 적 감지, 타겟정하기, 공격 실행 등 기능 구현 예정  
 public class Tower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float _range;          // 타워 사거리
+    public float _damage;         // 타워 공격력
+    public float _attackSpeed;    // 타워 공격속도
+    public float _towerheals; // 타워 체력
+    public enum TargetingMode { Nearest} // 가까운 몬스터 찾기 
+    private Transform _currentTarget;  // 현재 타겟
+
+    //☆★☆★ 테스트 프리팹이라 나중에 이거 다 바꿔야함 ☆★☆★
+    public GameObject _bulletPrefab; // 총알 프리팹 (test용) 
+    public Transform _firePoint; // 총알 발 사 위치
+    private float _attackTimer = 0f; // 공격 타이머 (에임 타이머 같이 활용 가능)
+
     void Start()
     {
+        _currentTarget = null;
+        
+        if (_range <= 0) _range = 5f; 
+        if (_damage <= 0) _damage = 10f; 
+        if (_attackSpeed <= 0) _attackSpeed = 1f;
+    }
+
+    void Update()
+    {
+        CheckTarget(); // 타겟이 범위 안에 있는지
+        FindTarget(); // 타겟 찾는 함수 진행
+        Attack(); // 공격 추가
         
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    // 만약에 타워가 공격하지 않는다면 타겟을 찾는다
+    // 공격을 안하면 타겟을 찾는다
+    // 공격 중엔 타겟을 차지 않는다 
+
+    public void FindTarget() //타겟 찾는 경로
     {
+        // 타겟을 찾자        
+        Debug.Log("타겟 찾는 로직 시작");
+
+        // 타겟이 있으면 찾지 않아
+        if(_currentTarget != null) 
+        {
+            return; 
+        }
+
+        // 범위 안에 있는 Collider들 다 찾아 
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _range);
         
+        foreach (Collider col in colliders)
+        {
+            //타겟이 Monster로 되어 있는 오브젝트만 찾는다
+            if (col.CompareTag("Monster"))
+            {
+                _currentTarget = col.transform;
+                break;
+            }
+        }
+        Debug.Log("타겟 찾는 로직 끝");
     }
+
+    //타겟이 죽거나 범위 밖으로 나갈 경우를 타겟을 해제한다 
+
+    private void CheckTarget()
+    {
+        // 타겟이 없으면 이 코드 진행할 필요 없어 (타겟이 죽거나 잡힌적 없거나)
+        // 이렇게 하면 타워랑 몬스터랑 같이 인식 될 거 같은데 ...음...
+        if (_currentTarget == null || _currentTarget.gameObject == null)
+        {
+            return;
+        }
+        
+        //타겟이 범위에서 나갔을때
+        float distrance = Vector3.Distance(transform.position, _currentTarget.position);
+        if (distrance > _range)
+        {
+            _currentTarget = null;
+        }
+    }
+
+    //타겟 공격
+    private void Attack()
+    {
+        // 타겟이 없으면 공격 안함
+        if (_currentTarget == null)
+        {
+            return;
+        }
+
+        // 공격 쿨타임 설정
+        _attackTimer += Time.deltaTime;
+        if (_attackTimer >= 0.5f / _attackSpeed)
+        {
+            // 발사 순간을 bullet에게 타겟을 넘겨줘야하는데
+            GameObject bullet = Instantiate(_bulletPrefab, _firePoint.position, Quaternion.identity);
+            // 총알을 타겟에게 주고 bullet 스크립트 완성 되어야함 완성되면 아래 내용 추가
+            bullet.GetComponent<Bullet>().SetTarget(_currentTarget, _damage);
+            _attackTimer = 0f; // 끝나면 타이머 초기화 -> 초기화 해야 나중에 0부터 계산함
+        }
+    }
+
+    
 }
