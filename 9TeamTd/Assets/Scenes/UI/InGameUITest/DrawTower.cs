9a8286d7eb46f7ef705f2b41d18b9edb61b117a7
Masttte <peonyak08@gmail.com>
2026-01-28 21:36:50 +0900

    큰맵 테스트 장애물 설정 실험
    아직 재작중 백업입니다

--- 9TeamTd/Assets/Scenes/UI/InGameUITest/DrawTower.cs
+++ 9TeamTd/Assets/Scenes/UI/InGameUITest/DrawTower.cs
@@ -7,7 +7,8 @@ public class DrawTower : MonoBehaviour // 타워 배치 UI와 상호작용
 {
     [SerializeField] GridSystem gridSystem;
     [SerializeField] GameObject[] TowerPrefabs;
-    [HideInInspector] public int targetTower;
+    [HideInInspector] public int targetTower; // UI에서 선택한 타워 인덱스
+    GameObject targetToRemove;
 
     Vector3 worldPoint; Cell cell;
     private void Update()
@@ -23,6 +24,18 @@ public class DrawTower : MonoBehaviour // 타워 배치 UI와 상호작용
                 Instantiate(TowerPrefabs[targetTower], gridSystem.CellToWorld(cell, y: 0f), Quaternion.identity);
             }
         }
+
+        // 타워 삭제
+        //if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        //{
+        //    if (gridSystem.RemoveTower(cell))
+        //    {
+        //        // 플레이어 위치에 있는 타워 찾기
+        //        Vector3 towerPosition = gridSystem.CellToWorld(cell, y: 0f);
+        //        targetToRemove = Find
+        //        Destroy(targetToRemove);
+        //    }
+        //}
     }
 
 }
