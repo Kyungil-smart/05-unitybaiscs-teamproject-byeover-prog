cbda6e83b8f85671caade477b2bd6df4d079d915
156Sheong <waladeus@gmail.com>
2026-01-30 13:06:00 +0900

    Json 매니저 및 예제 스크립트 수정

--- 9TeamTd/Assets/Scripts/Json/JsonManager.cs
+++ 9TeamTd/Assets/Scripts/Json/JsonManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.Jobs.LowLevel.Unsafe;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -15,7 +17,6 @@ public enum JsonType
     SceneData,  // 현재 테이블 없음
     BaseData,   // 현재 테이블 없음
     TowerData,
-    PlayerData, // 현재 테이블 없음
     MonsterData,    // 현재 테이블 없음
     ProjectileData,
     StatusEffectData,   // 현재 테이블 없음
@@ -46,7 +47,7 @@ public class JsonManager : MonoBehaviour
         {
             instanceJsonManger = this;
             DontDestroyOnLoad(gameObject);
-            LoadJsonFile(); // 파일 불러오기
+            LoadJsonFiles(); // 파일 불러오기
         }
         else
         {
@@ -55,14 +56,89 @@ public class JsonManager : MonoBehaviour
         #endregion
     }
 
-    /*
-    private void Start()
-    {
-        ChangeID(targetID, targetLevel);
-    }
-    */
 
-    // 이 함수를 통해
+    // 테이블 추가될 때마다 업데이트 필요
+    private void LoadJsonFiles()
+    {
+        LoadTowerData("Datas/TowerDataExample");
+        LoadProjectileData("Datas/ProjectileData");
+        /*
+        TextAsset jsonDataFile = Resources.Load<TextAsset>(dataFilePath);    // TextAsset(텍스트 파일 형식) 으로 리소스 폴더 하위 경로에서 TowerData 파일을 불러옴//TextAsset jsonDataFile = Resources.Load<TextAsset>("Datas/TowerData");    // TextAsset(텍스트 파일 형식) 으로 리소스 폴더 하위 경로에서 TowerData 파일을 불러옴
+        if (jsonDataFile != null)
+        {
+            _towerData = JsonUtility.FromJson<TowerDataList>(jsonDataFile.text);    // JsonUtility 활용해 TowerDataList 역직렬화
+        }
+        else
+        {
+            Debug.LogError("파일 없음");
+        }
+        */
+    }
+
+
+    // 테이블 추가될 때마다 업데이트 필요
+    private void LoadTowerData(string dataFilePath)
+    {
+        TextAsset jsonDataFile = Resources.Load<TextAsset>(dataFilePath);    // TextAsset(텍스트 파일 형식) 으로 리소스 폴더 하위 경로에서 TowerData 파일을 불러옴//TextAsset jsonDataFile = Resources.Load<TextAsset>("Datas/TowerData");    // TextAsset(텍스트 파일 형식) 으로 리소스 폴더 하위 경로에서 TowerData 파일을 불러옴
+        if (jsonDataFile != null)
+        {
+            _towerData = JsonUtility.FromJson<TowerDataList>(jsonDataFile.text);    // JsonUtility 활용해 TowerDataList 역직렬화
+        }
+        else
+        {
+            Debug.LogError($"파일 없음: {dataFilePath}");
+        }
+    }
+
+    private void LoadProjectileData(string dataFilePath)
+    {
+        TextAsset jsonDataFile = Resources.Load<TextAsset>(dataFilePath);
+        if (jsonDataFile != null)
+        {
+            _projectileData = JsonUtility.FromJson<ProjectileDataList>(jsonDataFile.text);
+        }
+        else
+        {
+            Debug.LogError($"파일 없음: {dataFilePath}");
+        }
+    }
+
+
+    // 각 데이터 별로 찾아서 리턴까지 하도록 기능 수정
+
+    public TowerDatas GetTowerData(int id, int level)
+    {
+        if (_towerData == null) return null; // 타워 데이터에 없으면 리턴
+
+        // TowerDataList에서 해당 레벨의 ID값의 캐릭터 찾기
+        TowerDatas foundData = _towerData.towers.Find(t => t.id == id && t.level == level);
+
+        if (foundData == null)
+        {
+            Debug.LogError($"ID: {id}, Level: {level} 에 해당하는 타워 데이터 없음");
+        }
+
+        return foundData; // 찾은 데이터를 호출한 곳으로 돌려줌
+    }
+
+
+    private ProjectileDatas GetProjectileData(int id)
+    {
+        if (_projectileData == null) return null;
+
+        ProjectileDatas foundData = _projectileData.projectiles.Find(t => t.id == id);
+
+        if (foundData == null)
+        {
+            Debug.LogWarning($"ID {id}에 해당하는 데이터 없음");
+        }
+
+        return foundData;
+    }
+
+
+    /*
+    // 이 함수를 통해 불러옴
     public void GetJsonRequest(JsonType type, string path, int id)
     {
         // 불러오기 전에 데이터 변경
@@ -97,30 +173,15 @@ public class JsonManager : MonoBehaviour
             case JsonType.TowerData:
                 ChangeTowerID(targetID, targetLevel);
                 break;
-            case JsonType.PlayerData: break;
             case JsonType.MonsterData: break;
             case JsonType.StatusEffectData: break;
             default: break;
         }
     }
+    
 
 
-    // 아래는 파일 조건에 따라 불러오는 함수
-
-    private void LoadJsonFile()
-    {
-        TextAsset jsonDataFile = Resources.Load<TextAsset>(dataFilePath);    // TextAsset(텍스트 파일 형식) 으로 리소스 폴더 하위 경로에서 TowerData 파일을 불러옴//TextAsset jsonDataFile = Resources.Load<TextAsset>("Datas/TowerData");    // TextAsset(텍스트 파일 형식) 으로 리소스 폴더 하위 경로에서 TowerData 파일을 불러옴
-        if (jsonDataFile != null)
-        {
-            _towerData = JsonUtility.FromJson<TowerDataList>(jsonDataFile.text);    // JsonUtility 활용해 TowerDataList 역직렬화
-        }
-        else
-        {
-            Debug.LogError("파일 없음");
-        }
-    }
-
-    /* 아래 ChangeID 기능은 현재 TowerDatas 만 읽을 수 있어서 수정 필요 */
+    
 
     // 원하는 ID와 레벨을 받아서 불러오는 용도
     // 타워, 몬스터, 기지 등에서 사용할 예정
@@ -167,4 +228,5 @@ public class JsonManager : MonoBehaviour
             Debug.LogWarning($"ID {id}에 해당하는 데이터 없음");
         }
     }
+    */
 }
