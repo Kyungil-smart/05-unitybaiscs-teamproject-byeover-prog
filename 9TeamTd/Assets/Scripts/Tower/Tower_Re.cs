a12611302d915776a5347d0f20b21a5356e8defe
156Sheong <waladeus@gmail.com>
2026-02-01 03:58:04 +0900

    타워 터렛 회전 버그 수정

--- 9TeamTd/Assets/Scripts/Tower/Tower_Re.cs
+++ 9TeamTd/Assets/Scripts/Tower/Tower_Re.cs
@@ -17,7 +17,7 @@ public class Tower_Re : MonoBehaviour
     public float _damage;         // 타워 공격력
     public float _attackSpeed;    // 타워 공격속도
     public float _towerheals; // 타워 체력
-    public enum TargetingMode { Nearest} // 가까운 몬스터 찾기 
+    public enum TargetingMode { Nearest } // 가까운 몬스터 찾기 
     [SerializeField] private Transform _currentTarget;  // 현재 타겟
 
     //☆★☆★ 테스트 프리팹이라 나중에 이거 다 바꿔야함 ☆★☆★
@@ -28,10 +28,10 @@ public class Tower_Re : MonoBehaviour
     void Start()
     {
         _currentTarget = null;  // 현재 타겟은 기본적으로 null 값임
-        
+
         // 0 이상이라면 ~
-        if (_range <= 0) _range = 5f;  
-        if (_damage <= 0) _damage = 10f; 
+        if (_range <= 0) _range = 5f;
+        if (_damage <= 0) _damage = 10f;
         if (_attackSpeed <= 0) _attackSpeed = 1f;
     }
 
@@ -40,10 +40,10 @@ public class Tower_Re : MonoBehaviour
         CheckTarget(); // 타겟이 범위 안에 있는지
         FindTarget(); // 타겟 찾는 함수 진행
         Attack(); // 공격 추가
-        
+
     }
 
-    
+
     // 만약에 타워가 공격하지 않는다면 타겟을 찾는다
     // 공격을 안하면 타겟을 찾는다
     // 공격 중엔 타겟을 차지 않는다 
@@ -54,20 +54,26 @@ public class Tower_Re : MonoBehaviour
         //Debug.Log("타겟 찾는 로직 시작");
 
         // 타겟이 있으면 찾지 않아
-        if(_currentTarget != null) 
+        if (_currentTarget != null)
         {
-            return; 
+            return;
         }
 
         // 범위 안에 있는 Collider들 다 찾아 
         Collider[] colliders = Physics.OverlapSphere(transform.position, _range);
-        
+
         foreach (Collider col in colliders)
         {
             //타겟이 Monster로 되어 있는 오브젝트만 찾는다
             if (col.CompareTag("Monster"))
             {
                 _currentTarget = col.transform;
+
+                // ## 타겟 찾으면 포탐 회전 동기화, 안전을 위해 if 문 안에 넣기
+                if (gameObject.GetComponent<TurretRotation>() != null)
+                {
+                    gameObject.GetComponent<TurretRotation>().SetTurretRotationTarget(_currentTarget);
+                }
                 break;
             }
         }
@@ -85,12 +91,18 @@ public class Tower_Re : MonoBehaviour
         {
             return;
         }
-        
+
         //타겟이 범위에서 나갔을때
         float distrance = Vector3.Distance(transform.position, _currentTarget.position);
         if (distrance > _range)
         {
             _currentTarget = null;
+
+            // ## 포탑 회전 스크립트 있으면 범위 나갔을때 null 값 주기
+            if (gameObject.GetComponent<TurretRotation>() != null)
+            {
+                gameObject.GetComponent<TurretRotation>().SetTurretRotationTarget(_currentTarget);
+            }
         }
 
         //Debug.Log("타겟 범위 찾기 완료");
@@ -110,15 +122,9 @@ public class Tower_Re : MonoBehaviour
         if (_attackTimer >= 1f / _attackSpeed)  // ## 유니티 여러 기능 기준이 초라서 1f로 수정함
         {
 
-            // ## 발사 직전에 포탑 회전, 안전을 위해 if 문 안에 넣기
-            if (gameObject.GetComponent<TurretRotation>() != null)
-            {
-                gameObject.GetComponent<TurretRotation>().SetTurretRotation(_currentTarget);
-            }
-            
 
-                // 발사 순간을 bullet에게 타겟을 넘겨줘야하는데
-                GameObject bullet = Instantiate(_bulletPrefab, _firePoint.position, Quaternion.identity);
+            // 발사 순간을 bullet에게 타겟을 넘겨줘야하는데
+            GameObject bullet = Instantiate(_bulletPrefab, _firePoint.position, Quaternion.identity);
 
             // 총알을 타겟에게 주고 bullet 스크립트 완성 되어야함 완성되면 아래 내용 추가
             // ## 안전을 위해 if 문 안으로 넣기
@@ -133,5 +139,5 @@ public class Tower_Re : MonoBehaviour
         }
     }
 
-    
+
 }
