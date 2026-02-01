5f27ec0fae705cc854436c4aaacfff39196fe565
hiyougg11 <164721514+nekioEmilia@users.noreply.github.com>
2026-02-01 18:07:31 +0900

    MonsterManager 스크립트 작성 완료

--- 9TeamTd/Assets/KYB_Folder/Scripts/MonsterManager.cs
+++ 9TeamTd/Assets/KYB_Folder/Scripts/MonsterManager.cs
@@ -38,7 +38,7 @@ public class MonsterManager : MonoBehaviour
 
     private void Start()
     {
-        
+        LoadLevelData();
     }
 
     public void LoadLevelData()
@@ -47,8 +47,7 @@ public class MonsterManager : MonoBehaviour
         lastSpawnTimeById.Clear();
         
         // JsonManager에서 스폰 데이터를 가져온다
-
-        for (int id = 1100; id <= 1110; id++)
+        for (int id = 1100; id <= 1110; id++) // << 엑셀에 적은 ID 범위에 맞게 수정
         {
             var data = JsonManager.instanceJsonManger.GetMonsterSpawnData(id);
 
@@ -57,5 +56,66 @@ public class MonsterManager : MonoBehaviour
                 currentLevelSchedule.Add(data);
             }
         }
+
+        gameTime = 0f;
+        isRunning = true;
+        Debug.Log($"[MonsterManager] 게임 시작, 총 {currentLevelSchedule}개의 스폰 패턴 가동");
+    }
+
+    private void Update()
+    {
+        
+    }
+    
+    // 몬스터 스폰 로직
+    private void SpawnMonster(int monsterId)
+    {
+        if (!GridSystem.Instance.TryGetRandomSpawnCell(out Cell spawnCell))
+        {
+            return;
+        }
+        
+        // 그리드 좌표 -> 월드 좌표 변환
+        Vector3 spawnPos = GridSystem.Instance.CellToWorld(spawnCell);
+        
+        // 기본 스탯 + 보상 정보 가져오기
+        MonsterDatas stats = JsonManager.instanceJsonManger.GetMonsterData(monsterId, 1);
+        MonsterResourcesDatas rewards = JsonManager.instanceJsonManger.GetMonsterResourcesData(monsterId);
+        
+        if (stats == null || rewards == null)
+        {
+            Debug.LogError($"[MonsterManager] ID {monsterId}의 데이터가 없습니다.");
+        }
+
+        Monster newMonster = GetFromPool();
+        newMonster.transform.position = spawnPos;
+        
+        // 몬스터 초기화
+        newMonster.Initialize(stats, rewards, baseTransform);
+
+    }
+
+    // 오브젝트 풀링
+    private Monster GetFromPool()
+    {
+        if (pool.Count > 0)
+        {
+            Monster m = pool.Pop();
+            m.gameObject.SetActive(true);
+            return m;
+        }
+        else
+        {
+            // 풀이 비었으면 새로 생성
+            Monster m = Instantiate(monsterPrefab, transform);
+            m.OnDeath += ReturnToPool;
+            return m;
+        }
+    }
+
+    public void ReturnToPool()
+    {
+        monster.gameObject.SetActive(false);
+        pool.Push(monster);
     }
 }
