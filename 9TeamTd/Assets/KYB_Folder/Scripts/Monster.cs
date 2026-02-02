314e3cead9d4777138a5e6defd411631c669c17d
156Sheong <waladeus@gmail.com>
2026-02-02 20:34:09 +0900

    몬스터 베이스 닿았을 때 버그 수정

--- 9TeamTd/Assets/KYB_Folder/Scripts/Monster.cs
+++ 9TeamTd/Assets/KYB_Folder/Scripts/Monster.cs
@@ -151,8 +151,9 @@ public class Monster : MonoBehaviour, IDamagable
             {
                 Debug.LogWarning("기지에 IDamageble 스크립트가 없습니다");
             }
-            
-            Die(isKilledByPlayer: false);
+
+            Destroy(gameObject);
+            // Die(isKilledByPlayer: false);
         }
     }
 }
