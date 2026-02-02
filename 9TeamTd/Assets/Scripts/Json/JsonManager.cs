d674537832fe9c8c5bd641532186e80fa9947f50
156Sheong <waladeus@gmail.com>
2026-02-02 14:28:38 +0900

    임시용 상태효과 기능 json 파일 및 연결 추가

--- 9TeamTd/Assets/Scripts/Json/JsonManager.cs
+++ 9TeamTd/Assets/Scripts/Json/JsonManager.cs
@@ -5,6 +5,7 @@ using System.IO;
 using Unity.Jobs.LowLevel.Unsafe;
 using Unity.VisualScripting;
 using UnityEngine;
+using static StatusEffectDatas;
 using static TowerStats;
 using static UnityEngine.GraphicsBuffer;
 
@@ -22,7 +23,7 @@ public enum JsonType
     MonsterResourcesData,
     MonsterSpawnData,
     ProjectileData,
-    StatusEffectData,   // 현재 테이블 없음
+    StatusEffectData,
 }
 
 // https://3dperson1.tistory.com/117 스크립트 기반 -> 추후 수정 예정
@@ -45,6 +46,7 @@ public class JsonManager : MonoBehaviour
     private MonsterResourcesDataList _monsterResourcesData;
     private MonsterSpawnDataList _monsterSpawnData;
     private ProjectileDataList _projectileData;
+    private StatusEffectDataList _statusEffectData;
 
 
     private void Awake()
@@ -73,7 +75,7 @@ public class JsonManager : MonoBehaviour
         LoadMonsterResourcesData("Datas/MonsterResourcesData");
         LoadMonsterSpawnData("Datas/MonsterSpawnData");
         LoadProjectileData("Datas/ProjectileData");
-
+        LoadProjectileData("Datas/StatusEffectData");
 
         /*
         TextAsset jsonDataFile = Resources.Load<TextAsset>(dataFilePath);    // TextAsset(텍스트 파일 형식) 으로 리소스 폴더 하위 경로에서 TowerData 파일을 불러옴//TextAsset jsonDataFile = Resources.Load<TextAsset>("Datas/TowerData");    // TextAsset(텍스트 파일 형식) 으로 리소스 폴더 하위 경로에서 TowerData 파일을 불러옴
@@ -99,6 +101,9 @@ public class JsonManager : MonoBehaviour
 
         return foundData != null;
     }
+
+
+
 
 
 
@@ -170,6 +175,21 @@ public class JsonManager : MonoBehaviour
             Debug.LogError($"파일 없음: {dataFilePath}");
         }
     }
+
+    private void LoadStatusEffectData(string dataFilePath)
+    {
+        TextAsset jsonDataFile = Resources.Load<TextAsset>(dataFilePath);
+        if (jsonDataFile != null)
+        {
+            _statusEffectData = JsonUtility.FromJson<StatusEffectDataList>(jsonDataFile.text);
+        }
+        else
+        {
+            Debug.LogError($"파일 없음: {dataFilePath}");
+        }
+    }
+
+
 
 
 
@@ -251,6 +271,24 @@ public class JsonManager : MonoBehaviour
 
         return foundData;
     }
+
+
+    public StatusEffectDatas GetStatusEffectData(int id)
+    {
+        if (_statusEffectData == null) return null;
+
+        StatusEffectDatas foundData = _statusEffectData.statusEffects.Find(t => t.id == id);
+
+        if (foundData == null)
+        {
+            Debug.LogWarning($"ID {id}에 해당하는 데이터 없음");
+        }
+
+        return foundData;
+    }
+
+
+
 
 
     /*
