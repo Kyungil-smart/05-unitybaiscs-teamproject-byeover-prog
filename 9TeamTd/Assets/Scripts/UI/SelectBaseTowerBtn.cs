1f03f5273d0218b46e76c812e4ae87666f03323c
Masttte <peonyak08@gmail.com>
2026-02-03 04:11:57 +0900

    [타이틀 기능추가] 스테이지, 기지 선택 UI

--- 9TeamTd/Assets/Scripts/UI/SelectBaseTowerBtn.cs
+++ 9TeamTd/Assets/Scripts/UI/SelectBaseTowerBtn.cs
@@ -1,53 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SelectBaseTowerBtn : MonoBehaviour
 {
-    //[Header("타워 정보")]
-    //[SerializeField] int targetID = 1000;
-    //[SerializeField] int targetLevel = 1;
-    //bool isSelected = false;
+    [Header("타워 정보")]
+    [SerializeField] int targetID = 1000;
+    [SerializeField] int targetLevel = 1;
 
-    //[Header("버튼 색상")]
-    //Color normalColor = Color.white;
-    //Color selectedColor = new Color(0.65f, 1f, 0.65f, 1f);
-    
-    //Image image;
-    //Button btn;
+    [Header("버튼 색상")]
+    Color normalColor = Color.white;
+    Color selectedColor = new Color(0.65f, 65f, 0.95f, 1f);
 
-    //private void Awake()
-    //{
-    //    image = GetComponent<Image>();
-    //    btn = GetComponent<Button>();
-    //}
+    Image image;
 
-    //private void Start()
-    //{
-    //    GameManager.Instance.SelectedBaseID.Value = targetID;
-    //}
+    [SerializeField] TextMeshProUGUI descText;
+    string desc;
 
-    //void OnSelectedBaseIDChanged(int value)
-    //{
-    //    // 선택된 기지 ID가 변경될 때 처리할 내용
-    //    GameManager.Instance.SelectedBaseID.Value = value;
 
-    //    if (value == targetID)
-    //    {
-    //        // 강조 표시
-    //        image.color = selectedColor;
-    //    }
-    //    else
-    //    {
-    //        image.color = normalColor;
-    //        isSelected = false;
-    //    }
-    //}
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
 
-    //public void OnClick()
-    //{
-    //    GameManager.Instance.SelectedBaseID.Value = targetID;
-    //    isSelected = true;
-    //}
+    private void Start()
+    {
+        GameManager.Instance.SelectedBaseID.Value = targetID;
+        GameManager.Instance.SelectedBaseID.OnValueChanged += OnSelectedBaseIDChanged;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.SelectedBaseID.OnValueChanged -= OnSelectedBaseIDChanged;
+    }
+
+    void OnSelectedBaseIDChanged(int value)
+    {
+        // 선택된 기지 ID가 변경될 때 처리할 내용
+
+        if (value == targetID)
+        {
+            // 강조 표시
+            image.color = selectedColor;
+            
+        }
+        else
+        {
+            image.color = normalColor;
+        }
+    }
+
+    public void OnClick()
+    {
+        GameManager.Instance.SelectedBaseID.Value = targetID;
+        ShowInfo(targetID, targetLevel);
+    }
+
+    public void ShowInfo(int targetID, int targetLevel)
+    {
+        TowerDatas data = JsonManager.instanceJsonManger.GetTowerData(targetID, targetLevel);
+
+        descText.text = data.desc;
+    }
 }
