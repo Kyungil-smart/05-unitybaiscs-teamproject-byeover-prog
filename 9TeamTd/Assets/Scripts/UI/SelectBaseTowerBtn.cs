01a0735cb37bc3db4a54df0c24d3078eda634db8
Masttte <peonyak08@gmail.com>
2026-02-03 03:30:43 +0900

    [백업] 중간저장

---
+++ 9TeamTd/Assets/Scripts/UI/SelectBaseTowerBtn.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectBaseTowerBtn : MonoBehaviour
+{
+    //[Header("타워 정보")]
+    //[SerializeField] int targetID = 1000;
+    //[SerializeField] int targetLevel = 1;
+    //bool isSelected = false;
+
+    //[Header("버튼 색상")]
+    //Color normalColor = Color.white;
+    //Color selectedColor = new Color(0.65f, 1f, 0.65f, 1f);
+    
+    //Image image;
+    //Button btn;
+
+    //private void Awake()
+    //{
+    //    image = GetComponent<Image>();
+    //    btn = GetComponent<Button>();
+    //}
+
+    //private void Start()
+    //{
+    //    GameManager.Instance.SelectedBaseID.Value = targetID;
+    //}
+
+    //void OnSelectedBaseIDChanged(int value)
+    //{
+    //    // 선택된 기지 ID가 변경될 때 처리할 내용
+    //    GameManager.Instance.SelectedBaseID.Value = value;
+
+    //    if (value == targetID)
+    //    {
+    //        // 강조 표시
+    //        image.color = selectedColor;
+    //    }
+    //    else
+    //    {
+    //        image.color = normalColor;
+    //        isSelected = false;
+    //    }
+    //}
+
+    //public void OnClick()
+    //{
+    //    GameManager.Instance.SelectedBaseID.Value = targetID;
+    //    isSelected = true;
+    //}
+}
