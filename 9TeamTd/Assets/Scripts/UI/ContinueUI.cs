c2a2b72edd0563d3826216567477cf57e4c7b6f9
156Sheong <waladeus@gmail.com>
2026-02-05 23:21:05 +0900

    타워 디펜스 개인적으로 기능 테스트중

--- 9TeamTd/Assets/Scripts/UI/ContinueUI.cs
+++ 9TeamTd/Assets/Scripts/UI/ContinueUI.cs
@@ -9,6 +9,7 @@ public class ContinueUI : MonoBehaviour
     [SerializeField] GameObject continueButton;
     [SerializeField] GameObject startButton;
     [SerializeField] Button stage2_startButton;
+    [SerializeField] Button stage3_startButton;
     [SerializeField] GameObject Lock1;
     [SerializeField] GameObject Lock2;
 
@@ -26,9 +27,23 @@ public class ContinueUI : MonoBehaviour
             if (_data.lastOpenStageNum > 1)
             {
                 stage2_startButton.interactable = true;
+
                 Lock1.SetActive(false);
+
+                stage3_startButton.interactable = true;
+
+                Lock2.SetActive(false);
+
+            }
+            /*
+            if (_data.lastOpenStageNum > 2)
+            {
+
+                stage3_startButton.interactable = true;
+
                 Lock2.SetActive(false);
             }
+            */
         }
     }
 
